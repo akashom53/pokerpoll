7daa332a0295b2aa21eee3523f755408819c4cc1
akashom53 <akashom53@gmail.com>
2020-04-29 23:58:43 +0530

    Updates

---
+++ precache-manifest.a6bbbdbcbd2f79120be4de4d4124cc8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9699382bb677e85cecf5589fbb5e3907",
+    "url": "/pokerpoll/index.html"
+  },
+  {
+    "revision": "54de2d0e0ad78afc7738",
+    "url": "/pokerpoll/static/css/main.a39ec89e.chunk.css"
+  },
+  {
+    "revision": "3bc409072e3262952554",
+    "url": "/pokerpoll/static/js/2.c3515680.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/pokerpoll/static/js/2.c3515680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54de2d0e0ad78afc7738",
+    "url": "/pokerpoll/static/js/main.89508aab.chunk.js"
+  },
+  {
+    "revision": "83c40b1abc82647eb02b",
+    "url": "/pokerpoll/static/js/runtime-main.18585f0d.js"
+  }
+]);
